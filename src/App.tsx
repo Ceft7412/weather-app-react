0928b51e5db478cdfbcccd2629836502aa3e429d
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-15 19:52:10 +0800

    @update Tracking the width of a letter and adding some pixels to it.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,22 @@ function App() {
   return (
     <>
       <main className="min-h-screen flex items-center justify-center">
-        <span></span>
+        <section className="flex gap-2 text-neutral-600 font-medium text-[30px]">
+          <span>Currently in </span>
+          <form action="" className="w-auto">
+            <input
+              type="text"
+              className="border-b border-neutral-600 resize-none whitespace w-10 outline-none"
+              onKeyPress={(e) => {
+                e.currentTarget.style.width =
+                  (e.currentTarget.value.length + 2) * 13 + "px";
+              }}
+            />
+          </form>
+          <span>, it's </span>
+        </section>
+        <section></section>
+        <section></section>
       </main>
     </>
   );
