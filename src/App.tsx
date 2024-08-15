f81c49a67259cf60874fce8bc542ea23306aa70c
Ceft7412 <146732550+Ceft7412@users.noreply.github.com>
2024-08-15 19:31:32 +0800

    @update Use VITE instead of REACT_APP when accessing .env file data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 function App() {
-  return <></>;
+  console.log(import.meta.env.VITE_WEATHER_API);
+  return (
+    <>
+      <main className="min-h-screen flex items-center justify-center">
+        <span></span>
+      </main>
+    </>
+  );
 }
 
 export default App;
